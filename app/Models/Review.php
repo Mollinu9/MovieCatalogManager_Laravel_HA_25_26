a18f7e5cb587ab9532e18c1ea9361839abe4aa10
Jake Muscat <jakemuscat05@gmail.com>
2026-01-08 17:21:30 +0000

    filled in review modal

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'movie_id',
+        'rating',
+        'comment',
+    ];
+
+    /**
+     * Get the user that wrote the review
+     */
+    public function user_author()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the movie that is being reviewed
+     */
+    public function movie_reviewed()
+    {
+        return $this->belongsTo(Movie::class);
+    }
 }
